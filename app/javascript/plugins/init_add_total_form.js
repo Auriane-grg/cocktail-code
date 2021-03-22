f4eebc353642c0bd712011d34503ec77382c9c86
auriane gregoire <auriane.gregoire@gmail.com>
2021-03-22 21:39:28 +0100

    v2 show

--- app/javascript/plugins/init_add_total_form.js
+++ app/javascript/plugins/init_add_total_form.js
@@ -2,10 +2,18 @@ const initAddTotalForm= () => {
   const btn = document.getElementById("btn-total-form");
   if (btn) {
     const instructionForm = document.getElementById("instruction-form");
+    const addingredient = document.getElementById("btn-add-ingredient");
     btn.addEventListener('click', () => {
       instructionForm.classList.remove("d-none");
+      const lastChild = document.querySelector("#instruction-form")
+      lastChild.scrollIntoView({behavior: 'smooth'})
     });
-  };
+    addingredient.addEventListener('submit', () => {
+      const lastnew = document.querySelector("#new-ingredient")
+      lastnew.scrollIntoView({behavior: 'smooth'})
+      instructionForm.classList.remove("d-none");
+    });
+  }
 }
 
 export { initAddTotalForm }
