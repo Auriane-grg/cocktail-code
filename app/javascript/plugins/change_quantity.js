bf0eb35de957c13257c0f4c7de99374fae59b8bc
auriane gregoire <auriane.gregoire@gmail.com>
2021-03-27 11:26:49 +0100

    if plus on show

--- app/javascript/plugins/change_quantity.js
+++ app/javascript/plugins/change_quantity.js
@@ -12,36 +12,38 @@ const changeQuantity= () => {
   const oneGlass = document.getElementById("one-glass");
   const severalGlass = document.getElementById("several-glasses");
   
+  if (plus) {
+    plus.addEventListener('click', () => {
+      i++;
+      input.value = i;
+      nbr = document.getElementById("recipe-counter").value;
+      spanToChange.forEach((spanChanged) => {
+        let i = allQuantities.findIndex( element => element === parseInt(spanChanged.innerText));
+        console.log(i);
+        spanChanged.style.display = "none";
+        spanChanged.nextSibling.innerText = "";
+        spanChanged.nextSibling.insertAdjacentText('beforeend', `${nbr * parseInt(allQuantities[i])}`);
+        if (i === 1) {
+        } else {
+          severalGlass.classList.remove('d-none');
+          oneGlass.classList.add('d-none');
+        }
+        });
+    });
 
-  plus.addEventListener('click', () => {
-    i++;
-    input.value = i;
-    nbr = document.getElementById("recipe-counter").value;
-    spanToChange.forEach((spanChanged) => {
-      let i = allQuantities.findIndex( element => element === parseInt(spanChanged.innerText));
-      console.log(i);
-      spanChanged.style.display = "none";
-      spanChanged.nextSibling.innerText = "";
-      spanChanged.nextSibling.insertAdjacentText('beforeend', `${nbr * parseInt(allQuantities[i])}`);
-      if (i === 1) {
-      } else {
-        severalGlass.classList.remove('d-none');
-        oneGlass.classList.add('d-none');
-      }
-      });
-  });
-
-  minus.addEventListener('click', () => {
-    i--;
-    input.value = i;
-    nbr = document.getElementById("recipe-counter").value;
-    spanToChange.forEach((spanChanged) => {
-      let i = allQuantities.findIndex( element => element === parseInt(spanChanged.innerText));
-      console.log(i);
-      spanChanged.style.display = "none";
-      spanChanged.nextSibling.innerText = "";
-      spanChanged.nextSibling.insertAdjacentText('beforeend', `${nbr * parseInt(allQuantities[i])}`);
-      });
-  });
+    minus.addEventListener('click', () => {
+      i--;
+      input.value = i;
+      nbr = document.getElementById("recipe-counter").value;
+      spanToChange.forEach((spanChanged) => {
+        let i = allQuantities.findIndex( element => element === parseInt(spanChanged.innerText));
+        console.log(i);
+        spanChanged.style.display = "none";
+        spanChanged.nextSibling.innerText = "";
+        spanChanged.nextSibling.insertAdjacentText('beforeend', `${nbr * parseInt(allQuantities[i])}`);
+        });
+    });
+  }
 }
+
 export { changeQuantity }
