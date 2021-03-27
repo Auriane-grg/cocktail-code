78992cb39283be89b8be7daf0eaa9c6ba00a9ceb
auriane gregoire <auriane.gregoire@gmail.com>
2021-03-27 16:55:16 +0100

    unity of measure of instruction not mandatory

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -3,7 +3,6 @@ class Instruction < ApplicationRecord
   belongs_to :ingredient
 
   validates :quantitynb, presence: true
-  validates :quantity, presence: true
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient,
   message: "should had 1 time each ingredient only" }
   validates :ingredient, presence: true
